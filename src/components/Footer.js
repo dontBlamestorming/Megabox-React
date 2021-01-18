327ba2643bdc8211ff61964000abf091e1078917
dave <lklone2005@gmail.com>
2021-01-18 17:48:29 +0900

    Refactoring Footer component and style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,40 @@
 import React from "react";
 import "../styles/footer.scss";
-
+import { footer__sns__info, footer__infor__info } from "../data";
 function Footer() {
+  const snsGenList = () => {
+    const list = footer__sns__info.map((info, i) => {
+      return (
+        <li className={`icon s${i + 1}`} key={i}>
+          <a href="#">
+            <span className="ir_pm">{info}</span>
+          </a>
+        </li>
+      );
+    });
+
+    return list;
+  };
+
+  const inforGenList = () => {
+    const list = footer__infor__info.map((info, i) => {
+      return (
+        <li key={i}>
+          <a href="#">{info}</a>
+        </li>
+      );
+    });
+
+    return list;
+  };
+
   return (
     <footer id="footer">
       <div id="footer_sns">
-        <div class="container">
-          <div class="footer_sns">
-            <ul>
-              <li class="icon s1">
-                <a href="#">
-                  <span class="ir_pm">트위터</span>
-                </a>
-              </li>
-              <li class="icon s2">
-                <a href="#">
-                  <span class="ir_pm">페이스북</span>
-                </a>
-              </li>
-              <li class="icon s3">
-                <a href="#">
-                  <span class="ir_pm">인스타그램</span>
-                </a>
-              </li>
-              <li class="icon s4">
-                <a href="#">
-                  <span class="ir_pm">구글 플레이</span>
-                </a>
-              </li>
-              <li class="icon s5">
-                <a href="#">
-                  <span class="ir_pm">아이폰 앱스토어</span>
-                </a>
-              </li>
-            </ul>
-            <div class="tel">
+        <div className="container">
+          <div className="footer_sns">
+            <ul>{snsGenList()}</ul>
+            <div className="tel">
               <a href="#">
                 ARS <em>1544-0070</em>
               </a>
@@ -43,44 +43,22 @@ function Footer() {
         </div>
       </div>
       <div id="footer_infor">
-        <div class="container">
-          <div class="row">
-            <div class="footer_infor">
+        <div className="container">
+          <div className="row">
+            <div className="footer_infor">
               <h2>
                 <img src="assets/img/logo_footer.png" alt="megabox" />
               </h2>
-              <ul>
-                <li>
-                  <a href="#">회사소개</a>
-                </li>
-                <li>
-                  <a href="#">채용정보</a>
-                </li>
-                <li>
-                  <a href="#">제휴/광고/부대사업 문의</a>
-                </li>
-                <li>
-                  <a href="#">이용약관</a>
-                </li>
-                <li>
-                  <a href="#">개인정보처리방침</a>
-                </li>
-                <li>
-                  <a href="#">고객센터</a>
-                </li>
-                <li>
-                  <a href="#">윤리경영</a>
-                </li>
-              </ul>
+              <ul>{inforGenList()}</ul>
               <address>
                 <p>
                   서울특별시 강남구 도산대로 156, 2층 메가박스중앙(주) (논현동,
                   중앙엠앤비사옥)
                   <br />
-                  <span class="bar2">대표자명 김진선</span> 개인정보보호 책임자
-                  경영지원실 실장 박영진
+                  <span className="bar2">대표자명 김진선</span> 개인정보보호
+                  책임자 경영지원실 실장 박영진
                   <br />
-                  <span class="bar2">사업자등록번호 211-86-59478</span>{" "}
+                  <span className="bar2">사업자등록번호 211-86-59478</span>{" "}
                   통신판매업신고번호 제 833호
                 </p>
                 <p>Copyright 2014 by MegaboxJoongAng Inc. All right reserved</p>
