8097d470bb1b912b51e07f011523a4969745afa3
dave <lklone2005@gmail.com>
2021-01-19 00:41:59 +0900

    Add components data for JSX

--- src/data.js
+++ src/data.js
@@ -140,3 +140,105 @@ export const new__inputTime = [
   "오후 11:00",
   "오후 12:00",
 ];
+
+export const help__allNotice__info = [
+  {
+    coverage: "전체",
+    date: "2021.01.03",
+    content:
+      "[무대인사] (뺑반) 1주차~ (뺑반) 1주차~ (뺑반) 1주차~ (뺑반) 1주차~ (뺑반) 1주차~",
+  },
+  {
+    coverage: "강남",
+    date: "2021.01.12",
+    content:
+      "[라이브뷰잉] 플라잉독 10주년 기념 라이브 10주년 기념라이브 10주년 기념 라이브..",
+  },
+  {
+    coverage: "안양",
+    date: "2021.01.17",
+    content:
+      "[무대인사] (내 안의 그놈) 1주차 1주차 1주차 1주차1주차 1주차 1주차 1주차",
+  },
+  {
+    coverage: "전체",
+    date: "2021.01.25",
+    content:
+      "[공지] 2019년 1월 VIP 미션 이벤트 1월 VIP 미션이벤트 1월 VIP 미션 이벤트",
+  },
+];
+
+export const help__movieNotice__info = [
+  {
+    coverage: "전체",
+    date: "2021.01.03",
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    coverage: "강남",
+    date: "2021.01.12",
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    coverage: "안양",
+    date: "2021.01.17",
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    coverage: "전체",
+    date: "2021.01.13",
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
+export const help__card__info = [
+  {
+    title: "T멤버십 일반",
+    desc: "영화 2,000원 현장 즉시 할인(1일 5매)",
+  },
+  {
+    title: "LG U+",
+    desc: "영화 2,000원 현장 즉시 할인(1일 5매)",
+  },
+  {
+    title: "SK멤버십 일반",
+    desc: "영화 2,000원 현장 즉시 할인(1일 5매)",
+  },
+];
+
+export const help__service__info = [
+  {
+    svg: "mail",
+    desc: "1:1 문의",
+  },
+  {
+    svg: "joystick",
+    desc: "단체관람",
+  },
+  {
+    svg: "faq",
+    desc: "자주묻는 질문",
+  },
+  {
+    svg: "phone",
+    desc: "분실물 문의",
+  },
+];
+
+export const footer__sns__info = [
+  "트위터",
+  "페이스북",
+  "인스타그램",
+  "구글 플레이",
+  "아이폰 앱스토어",
+];
+
+export const footer__infor__info = [
+  "회사소개",
+  "채용정보",
+  "제휴 / 광고 / 부대사업 문의",
+  "이용약관",
+  "개인정보처리방침",
+  "고객센터",
+  "윤리경영",
+];
